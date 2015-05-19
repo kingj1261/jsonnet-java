3817cb23cc0cd2f0b9beac342ce38e2c618764e6
Roman Bondarchuk <rmnbnd@gmail.com>
2015-05-19 22:36:53 +0300

    Added empty test and template jsonnet file

---
+++ src/test/java/json/execute/ExecutorTest.java
@@ -0,0 +1,14 @@
+package json.execute;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ExecutorTest {
+
+    @Test
+    public void shouldReturnStringJson() throws IOException {
+    }
+
+}
