4e7889b6006418026f7bbc12d3b502e474f86552
Roman Bondarchuk <rmnbnd@gmail.com>
2015-05-19 23:00:36 +0300

    Added entity State for the parse number

---
+++ src/main/java/json/execute/entity/State.java
@@ -0,0 +1,12 @@
+package json.execute.entity;
+
+public enum State {
+    BEGIN,
+    AFTER_ZERO,
+    AFTER_ONE_TO_NINE,
+    AFTER_DOT,
+    AFTER_DIGIT,
+    AFTER_E,
+    AFTER_EXP_SIGN,
+    AFTER_EXP_DIGIT
+}
